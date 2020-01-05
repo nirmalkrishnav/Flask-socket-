2bed298f39a7457dc5d0658262ddaa3ba3751ad4
unknown <nirmalkrishnav@gmail.com>
2020-01-05 10:21:39 +0530

    added text file to send it to html

--- main.py
+++ main.py
@@ -5,17 +5,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 socketio = SocketIO(app)
 
+@app.route('/messages')
+def messages():
+    return render_template('session.html')
+
 @app.route('/')
 def sessions():
-    return render_template('session.html')
+    text = open('data.txt', 'r+')
+    content = text.read()
+    text.close()
+    return render_template('data.html', text=content)
 
 def messageReceived(methods=['GET', 'POST']):
     print('message was received!!!')
+
 
 @socketio.on('my event')
 def handle_my_custom_event(json, methods=['GET', 'POST']):
     print('received my event: ' + str(json))
     socketio.emit('my response', json, callback=messageReceived)
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
